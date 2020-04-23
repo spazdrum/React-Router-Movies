1197d13b0b72b08fd259b48669c57fde84aa8f61
spazdrum <spazdrum@gmail.com>
2020-04-23 00:13:31 -0400

    completed MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Movie from './Movies/Movie';
 import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
@@ -30,9 +30,11 @@ const App = () => {
   return (
     <div>
       <Router>
-        <Route path="/" component={MovieList} />
-        <Route path = "/movies/:id" component={Movie} />
         <SavedList list={savedList} />
+        <Switch>
+          <Route exact path="/" component={MovieList} />
+          <Route path = "/movies/:id" component={Movie} />
+        </Switch>
       </Router>
     </div>
   );
